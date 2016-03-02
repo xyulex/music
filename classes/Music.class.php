43801cdf34d97879d542276dd720c2ac9fe17c79
xyulex <raulmartinez911@hotmail.com>
2016-03-02 13:44:13 +0100

    Changes

--- classes/Music.class.php
+++ classes/Music.class.php
@@ -20,8 +20,7 @@ class Music {
 
     // Get gigs.
     public function getCover($gig_id) {
-        if (file_exists('/xampp/htdocs/music/covers/'.$gig_id.'.jpg') ||
-            file_exists('/home/xyulex/www/music/covers/'.$gig_id.'.jpg')){
+        if (file_exists('../covers/'.$gig_id.'.jpg')) {
             return 'green';
         }
         return 'red';
@@ -30,9 +29,9 @@ class Music {
     public function getCurrency($gig_date) {
         if (strtotime($gig_date)<strtotime('2002/01/01')) {
                 $currency = 'pta.';
-            } else{
+        } else {
                 $currency = '€';
-            }
+        }
         return $currency;
     }
 
@@ -43,20 +42,25 @@ class Music {
         $sql = 'select gig_bands as bands from gigs';
         $rows = $this->db()->query($sql);
 
-        while($row = $this->db()->fetch_array($rows)){
+        while($row = $this->db()->fetch_array($rows)) {
             $bandsArrayRow = explode('+', $row['bands']);
 
             foreach ($bandsArrayRow as $band) {
                 $bandTrimmed = trim($band);
-                if(!in_array($bandTrimmed, $bandsArray) && (!strpos($bandTrimmed,':'))) {
-                    $bandsArray[] = utf8_encode($bandTrimmed);
+                if(!in_array(utf8_encode($bandTrimmed), $bandsArray)) {
+                    if (strpos($bandTrimmed,':')) {
+                        $bandTrimmedArray = explode(":", $bandTrimmed);
+                        $bandsArray[] = utf8_encode(trim($bandTrimmedArray[1]));
+                    } else {
+                        $bandsArray[] = utf8_encode($bandTrimmed);
+                    }
                 }
             }
         }
 
-        $sql = 'select count(*) as gigs from gigs';
+        $sql   = 'select count(*) as gigs from gigs';
         $rows2 = $this->db()->query($sql);
-        $row2 = $this->db()->fetch_array($rows2);
+        $row2  = $this->db()->fetch_array($rows2);
 
         $stats['gigs']  = $row2;
         $stats['bandsNumber'] = count($bandsArray);
@@ -66,7 +70,7 @@ class Music {
 
     public function getGigs($filter = '') {
         $where = '';
-        if ($filter){
+        if ($filter) {
             $where = " where type =" . $filter;
         }
 
@@ -74,19 +78,19 @@ class Music {
         $rows = $this->db()->query($sql);
 
 
-        while($row = $this->db()->fetch_array($rows)){
+        while ($row = $this->db()->fetch_array($rows)) {
           $currentGig = array();
-          $currentGig['gig_id'] = $row['gig_id'];
-          $currentGig['gig_date'] = $row['gig_date'];
-          $currentGig['gig_bands'] = utf8_encode($row['gig_bands']);
-          $currentGig['gig_venue'] = utf8_encode($row['gig_venue']);
-          $currentGig['gig_price'] = $row['gig_price'];
+          $currentGig['gig_id']     = $row['gig_id'];
+          $currentGig['gig_date']   = $row['gig_date'];
+          $currentGig['gig_bands']  = utf8_encode($row['gig_bands']);
+          $currentGig['gig_venue']  = utf8_encode($row['gig_venue']);
+          $currentGig['gig_price']  = $row['gig_price'];
           $gigs[] = $currentGig;
         }
 
         $table = '<table id="gigsTable" class="display"><thead><th>Fecha</th><th>Grupos</th><th>Sala</th><th>Precio</th></thead><tbody>';
 
-        foreach($gigs as $gig) {
+        foreach ($gigs as $gig) {
             $bgcolor = $this->getCover($gig['gig_id']);
             $table .= '<tr><td >'.$gig['gig_date'].'</td><td><a href="detail.php?gig_id='.$gig['gig_id'].'" style="color:'.$bgcolor.'">'.$gig['gig_bands'].'</a><b>---'.$gig['gig_id'].'</b></td><td>'.$gig['gig_venue'].'</td><td>'.$gig['gig_price'].'</td></tr>';
         }
@@ -99,7 +103,7 @@ class Music {
     public function getGigsDetail($gig_id) {
         $sql = "SELECT * FROM gigs where gig_id=" . $gig_id;
         $rows = $this->db()->query($sql);
-        while($gig = $this->db()->fetch_array($rows)){
+        while($gig = $this->db()->fetch_array($rows)) {
             $currency = $this->getCurrency($gig['gig_date']);
 
             echo
@@ -119,7 +123,7 @@ class Music {
     // Grabar banda o sala.
     public function addGig($date, $bands, $venue, $price) {
         $data = new stdClass();
-        $data->gig_date = utf8_decode($date);
+        $data->gig_date  = utf8_decode($date);
         $data->gig_bands = utf8_decode($bands);
         $data->gig_venue = utf8_decode($venue);
         $data->gig_price = $price;
--- templates/bands.php
+++ templates/bands.php
@@ -1,4 +1,3 @@
-
 <?php require_once('mainbar.php');
 global $MUSIC;
 $stats = $MUSIC->getStatistics();
